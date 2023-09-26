f6aa1327e9adc69c96bb5c133c4ff7fb709e574d
Rebecca Nangila <srebeccanangila@gmail.com>
2023-09-26 11:47:55 +0300

    Created the onboarding screens

--- Lactomama/app/src/main/java/com/ajolla/lactomama/UserJourneyActivity.kt
+++ Lactomama/app/src/main/java/com/ajolla/lactomama/UserJourneyActivity.kt
@@ -1,11 +1,36 @@
 package com.ajolla.lactomama
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.ajolla.lactomama.databinding.ActivityUserJourneyBinding
 
 class UserJourneyActivity : AppCompatActivity() {
+    lateinit var binding: ActivityUserJourneyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_journey)
+        binding = ActivityUserJourneyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val lactationButton = findViewById<Button>(R.id.lactationButton)
+        val motherButton = findViewById<Button>(R.id.motherButton)
+
+        // Set an OnClickListener for the Lactation Specialist button
+        lactationButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Set an OnClickListener for the Mother button
+        motherButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 }
