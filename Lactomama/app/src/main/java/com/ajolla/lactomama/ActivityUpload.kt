afd281ea88901b36b493bdd095e982adf714c6c2
KABANYANA <joyeusekabanyana@gmail.com>
2023-09-26 20:31:49 +0300

    Created the Lisence upload journey

---
+++ Lactomama/app/src/main/java/com/ajolla/lactomama/ActivityUpload.kt
@@ -0,0 +1,11 @@
+package com.ajolla.lactomama
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityUpload : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+    }
+}
